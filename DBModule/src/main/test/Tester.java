3575a54c172b674e73df4184e28f98939b76bcf4
xuBi-n <99213248@qq.com>
2020-12-09 11:03:05 +0800

    修改了DBUtil类

---
+++ DBModule/src/main/test/Tester.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.Connection;
+
+public class Tester {
+    @Test
+    public void Test1(){
+        ApplicationContext ctx=new ClassPathXmlApplicationContext("springDB.xml");
+        Connection conn= (Connection) ctx.getBean("conn");
+        System.out.println(conn);
+    }
+}
